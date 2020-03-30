0a0c458423f977a62c3d8e3109a740a8d9a2de19
Ignatius Agus <ignatiusagus@Ignatiuss-MacBook-Pro.local>
2020-03-30 11:38:16 +0700

    App.js is working now

---
+++ server/app.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
+
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+
+app.get('/', (req, res)=> res.send("fancy to-do"))
+
+app.listen(port, ()=>console.log(`listening on port ${port}`))
