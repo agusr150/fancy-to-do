0a28b750c69d4cec9ad8ad647cec6cde2be3e187
Ignatius Agus <ignatiusagus@Ignatiuss-MacBook-Pro.local>
2020-04-01 11:22:33 +0700

    start client

--- server/routes/APIrouter.js
+++ server/routes/APIrouter.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 const axios =require('axios')
 
-router.get('/current_weather', function (req, res){
+router.get('/current_weather', function (req, res){ 
     axios({
         method: 'get',
         url: 'http://api.openweathermap.org/data/2.5/weather?q=Jakarta,Indonesia&APPID=d2606a9ee3680c51b60d1b000b2647f4',
@@ -15,5 +15,34 @@ router.get('/current_weather', function (req, res){
         res.status(400).json(err)
     })
 })
+
+// router.post('/email', function (req, res){
+//     axios({
+//         method: 'post',
+//         url: 'https://mandrillapp.com/api/1.0/messages/send.json',
+//         data: {
+//           key: 'e543fd35687fac4132794add8cfe5681-us19',
+//           message: {
+//             from_email: 'ignatiusagus150@gmail.com',
+//             to: [
+//                 {
+//                   email: 'agusr150@yahoo.co.id',
+//                   name: 'agus',
+//                   type: 'to'
+//                 }
+//               ],
+//             autotext: true,
+//             subject: 'tessst',
+//             html: 'test email'
+//           }
+//         }
+//        })
+//        .then(data=>{
+//             res.status(200).json('sukses')
+//        })
+//        .catch(err=>{
+//            res.status(400).json(err)
+//        })
+//     })
 
 module.exports = router
